e8be48768455263875144d4d8ed3b6163fb01def
Niall Cooling <niall.cooling@gmail.com>
2019-08-02 11:43:40 +0100

    Debug=False

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'u)4yy8u)4g++w61)$vr32y3!39%+w97c1+&f1(h0^by=p2fb*v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
